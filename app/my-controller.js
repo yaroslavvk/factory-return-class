768cccb64f0d4ec277d83d568af175f82b3429d4
yvk <yaroslavvk@outlook.com>
2016-02-01 11:25:05 +0200

    correct

--- app/my-controller.js
+++ app/my-controller.js
@@ -11,7 +11,7 @@ angular.module('myApp')
             age: '28'
         };
 
-        var serializerDefault = true; // if true - parameters will transfer like "?param1=n&param2=m", else "/param1/n/param2/m"
+        var serializerDefault = true; // if 'true' - parameters will transfer like "?param1=n&param2=m", if 'false' - "/param1/n/param2/m"
 
         var service = new Service(params, data, serializerDefault);
 
